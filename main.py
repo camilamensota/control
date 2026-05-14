3f04b267c5a4bea714032a47844c3b5b278877b5
camilamensota <kmila400@gmail.com>
2026-05-14 10:41:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,104 @@
-import customtkinter as ctk # importamos la librería para la interfaz de usuario
+import customtkinter as ctk #libreria para el gui
 
-#Comenzaremos a realizar la interfaz de usuario
+#establece los colores de la interfaz
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
-#crearemos la ventana para la interfaz que sera de 800*500
+#crea la ventana de la interfaz
 app = ctk.CTk()
-app.geometry("2500*2500")
+app.geometry("900x600")
 app.title("Control de gastos personales")
 
-#creamos el titulo
+#creamos una caja de encabezado
+header_frame = ctk.CTkFrame(app)
+header_frame.pack(
+    fill="x",
+    pady=10
+)
+
+#titulo y formato de la ventana
 titulo = ctk.CTkLabel(
     app,
     text="Control de gastos personales",
-    font=("Arial", 28)
+    font=("Arial", 30)
 )
-
 titulo.pack(pady=20)
 
-#primero se pregunta los ingresos
-entry_fecha_ingreso = ctk.CTkL
-
-#aca haremos el campo para que el ususario ingrese el salario diario
-entry_salario = ctk.CTkEntry(
+#aca va el guardado del registro de ingresos, gastos y saldo actual
+label_ingreso = ctk.CTkLabel(
     app,
-    placeholder_text="Salario diario"
+    text="Ingresos: $0",
+    font=("Arial",24)
 )
-
-entry_salario.pack(pady=10)
-
-#creamos el boton para calcular
-boton = ctk.CTkButton(
+label_ingreso.pack(pady=10)
+#gasto
+label_gastos = ctk.CTkLabel(
     app,
-    text="Calcular"
+    text="Gastos: $0",
+    font=("Arial",24)
 )
+label_gastos.pack(pady=10)
+#saldo actual
+label_saldo = ctk.CTkLabel(
+    app,
+    text="Saldo Actual: $0",
+    font=("Arial", 24)
+)
+label_saldo.pack(pady=10)
 
-boton.pack(pady=10)
+#ComboBox nos sirve para elegir entre varias opciones sin tener que teclearlo, aca solo daremos opcion de ingreso o gasto
+combo_tipo = ctk.CTkComboBox(
+    app,
+    values=["Ingreso", "Gasto"],
+    width=300
+)
+combo_tipo.pack(pady=10)
 
-#se ejecuta la ventana
+#agragamos una categoria para que el usuario tenga un conocimiento de en donde gasta su dinero
+entry_categoria = ctk.CTkEntry(
+    app,
+    placeholder_text="Categoría",
+    width=300
+)
+entry_categoria.pack(pady=10)
+
+#espacio para que el usuario ingrese su gasto o su ingreso
+entry_monto = ctk.CTkEntry(
+    app,
+    placeholder_text="Monto",
+    width=300
+)
+entry_monto.pack(pady=10)
+
+#aca el usuario puede dar una descripcion de lo que ingreso o gasto
+entry_descripcion = ctk.CTkEntry(
+    app,
+    placeholder_text="Descripción",
+    width=300
+)
+entry_descripcion.pack(pady=10)
+
+#guarda la información de los movimientos
+btn_guardar = ctk.CTkButton(
+    app,
+    text="Guardar movimiento"
+)
+btn_guardar.pack(pady=15)
+
+label_historial = ctk.CTkLabel(
+    app,
+    text="Historial",
+    font=("Arial",15)
+)
+label_historial.pack(pady=5)
+
+#le da al usuario un historial de todo lo que lleva
+textbox = ctk.CTkTextbox(
+    app,
+    width=700,
+    height=200
+)
+textbox.pack(pady=10)
+
+#ejecuta la ventana
 app.mainloop()
-
-
-
